ebd98a346af345fe61af9251bec45c4741c53410
vkellers <Aces66High>
2015-09-17 17:17:13 +0200

    Introduce the event mechanism, so items don't have to be aware of state changes along with each cycle

--- src/main/java/com/vincentks/vic/game/Item.java
+++ src/main/java/com/vincentks/vic/game/Item.java
@@ -2,7 +2,7 @@ package com.vincentks.vic.game;
 
 import java.util.UUID;
 
-// TODO add definition of each of the interfaces in this hierarchyx
+// TODO add definition of each of the interfaces in this hierarchyxy
 public interface Item
 {
   Effort buildEffort();
